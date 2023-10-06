bb7b50ee70e77fa9bd2b9bbe0f60fc71850a323b
Pig <min208406@naver.com>
2023-10-06 19:51:00 +0900

    알고 과제 시작

--- 20201492_3.cpp
+++ 20201492_3.cpp
@@ -7,7 +7,7 @@ int main(void)
 {
     int n, m; // 4자리 정수를 받을 개수 n, 실행할 횟수 m
 
-    cout << "n과 m을 입력하세요: ";
+    cout << "n과 m을 입력하세요: ";//
     cin >> n >> m;
     srand((unsigned int)time(NULL));
     for (int i = 0; i < n; i++)
