bd2fde0780dd44514f1642f066c758697652ae6b
Pig <min208406@naver.com>
2023-10-10 15:49:23 +0900

    이우혁 코드ㅋㅋ

--- hello.cpp
+++ hello.cpp
@@ -1,211 +1,161 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <stdlib.h>
+#include <time.h>
+#include <cmath>
 
 using namespace std;
+int n, m;
 
-class Node // 노드
+// 연결리스트를 만들기 위한 구조체
+struct node
 {
-
-private:
-    int data;
-    Node *link;
-
-public:
-    Node(int data)
-    {
-        this->data = data;
-        this->link = nullptr;
-    }
-
-    void addNode(int data)
-    {
-        this->link = new Node(data);
-    }
-
-    Node *getNext()
-    {
-        return link;
-    }
-
-    int getData()
-    {
-        return data;
-    }
-
-    void setNext(Node *nextNode)
-    {
-        this->link = nextNode;
-    }
+    int num;
+    node *nextNode;
 };
 
-Node *stepOfRadixSort(Node *cursor, int divisor)
+// 값을 받아 새로운 노드를 생성해서 현재 노드에 연결해주는 함수
+node *makeAndLinkNode(node *currentNode, int num)
 {
-    Node *head[10] = {}; // 3)헤드를 저장하는 포인터 배열
-    Node *tail[10] = {}; // 4)테일을 저장하는 포인터 배열
-    Node *temp = nullptr;
-    Node *newHead;
+    //new 연산자 사용
+    node *newNode = new node;
+    newNode->num = num;
+    newNode->nextNode = nullptr;
 
-    Node *firstHead = cursor;
-
-    while (cursor != nullptr) // 2)자릿수에 해당하는 연결리스트 동적 메모리 할당
+    if (currentNode != nullptr)
     {
-
-        int radix = cursor->getData() / divisor % 10;
-
-        if (head[radix] == nullptr)
-        {
-            head[radix] = new Node(cursor->getData()); // 2)헤드 연결
-            tail[radix] = head[radix];                 // 2)테일 연결
-        }
-        else
-        {
-            tail[radix]->addNode(cursor->getData());
-            tail[radix] = tail[radix]->getNext();
-        }
-        cursor = cursor->getNext();
+        currentNode->nextNode = newNode;
     }
-
-    for (int i = 0; i < 10; i++) // 정렬된 연결리스들 연결
-    {
-        if (head[i] == nullptr)
-        {
-            continue;
-        }
-        else
-        {
-            if (temp != nullptr)
-            {
-                temp->setNext(head[i]);
-                temp = tail[i];
-            }
-            else
-            {
-                temp = tail[i];
-            }
-        }
-    }
-
-    for (int i = 0; i < 10; i++) // 새로운 연결 리스트의 head
-    {
-        if (head[i] != nullptr)
-        {
-            newHead = head[i];
-            break;
-        }
-    }
-
-    while (firstHead != nullptr) // 동적 메모리 할당 해제
-    {
-        Node *remover = firstHead;
-        firstHead = firstHead->getNext();
-        delete remover;
-    }
-
-    return newHead;
+    return newNode;
 }
 
-int maxRadixCount(Node *cursor)
+int main(void)
 {
-    int max = 1;
-    int data;
-    int temp;
+    srand(time(NULL));
 
-    while (cursor != nullptr)
-    {
-        data = cursor->getData();
-        if (data / 10 == 0)
-        {
-            cursor = cursor->getNext();
-        }
-        else
-        {
-            temp = 1;
-            while (data / 10 != 0)
-            {
-                data = data / 10;
-                temp++;
-            }
-            if (temp > max)
-            {
-                max = temp;
-            }
-            cursor = cursor->getNext();
-        }
-    }
-    return max;
-}
-
-Node *radixSort(Node *head)
-{
-    int maxRadix;
-    int digit = 1;
-    Node *newHead = head;
-
-    maxRadix = maxRadixCount(head);
-
-    for (int i = 0; i < maxRadix; i++)
-    {
-        newHead = stepOfRadixSort(newHead, digit);
-        digit = digit * 10;
-    }
-    return newHead;
-}
-
-int main()
-{
-    srand((unsigned int)time(NULL));
-    int n;
-    int m;
-    int j = 0;
-
-    cout << "n: ";
+    cout << "n:";
     cin >> n;
-
-    cout << "m: ";
+    cout << "m:";
     cin >> m;
 
-    while (j < m)
+    for (int i = 0; i < m; i++)
     {
-        int i = 1;
-        Node *head = new Node(rand() % 10000); // 1)동적 메모리 할당을 통한 연결리스트 생성
-        Node *cursor = head;
-        Node *radixSortCompleted;
+        node *randHead = makeAndLinkNode(nullptr, -1);
+        node *currentNode = randHead;
 
-        while (i < n) // 1)동적 메모리 할당을 통한 연결리스트 생성
+        // 각 연결리스트의 헤드를 저장하는 크기가 10인 포인터 배열을 생성
+        node *numHead[10] = {nullptr};
+
+        // 각 연결리스트의 테일을 저장하는 크기가 10인 포인터 배열을 생성
+        node *numTail[10] = {nullptr};
+
+        // 랜덤 숫자 생성, 동적 메모리 할당을 통한 연결리스트 생성
+        for (int j = 0; j < n; j++)
         {
-            cursor->addNode(rand() % 10000);
-            cursor = cursor->getNext();
-            i++;
+            int a = rand() % 10000;
+            currentNode = makeAndLinkNode(currentNode, a);
         }
-
-        cursor = head; // 연결리스트 출력
-        cout << "정렬 전: ";
-        while (cursor != nullptr)
+        cout<<"정렬 전->";
+        currentNode = randHead->nextNode;
+        while (currentNode != nullptr)
         {
-            cout << cursor->getData() << " ";
-            cursor = cursor->getNext();
+            cout << currentNode->num << " ";
+            currentNode = currentNode->nextNode;
         }
-        cout << endl;
+        cout << "\n";
 
-        radixSortCompleted = radixSort(head);
+        // 기수정렬 시행
 
-        cursor = radixSortCompleted; // 연결리스트 출력
-        cout << "정렬 후: ";
-        while (cursor != nullptr)
+        for (int j = 1; j < 5; j++)
         {
-            cout << cursor->getData() << " ";
-            cursor = cursor->getNext();
+
+            // 각 자릿수에 해당하는 10개의 연결리스트를 동적 메모리 할당으로 생성
+            for (int j = 0; j < 10; j++)
+            {
+                numHead[j] = new node;
+                numHead[j]->num = -1;
+                numHead[j]->nextNode = nullptr;
+                numTail[j] = numHead[j];
+            }
+
+            //몇번째 링크드리스트에 들어갈건지 구하기
+            currentNode = randHead->nextNode;
+            int nanoogi = pow(10, j);
+            int makeNanoogi = pow(10, j - 1);
+            while (currentNode != nullptr)
+            {
+                if (currentNode->num == -1)
+                {
+                    currentNode = currentNode->nextNode;
+                    continue;
+                }
+                int nodeWhere = (currentNode->num) % nanoogi / makeNanoogi;
+                makeAndLinkNode(numTail[nodeWhere], currentNode->num);
+                numTail[nodeWhere] = numTail[nodeWhere]->nextNode;
+                numTail[nodeWhere]->nextNode = nullptr;
+                currentNode = currentNode->nextNode;
+            }
+
+
+
+            // tail과 head 연결
+            node *saveNode = makeAndLinkNode(nullptr, -1);
+            node *saveCurrent = saveNode;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (numTail[i]->num == -1)
+                {
+                    continue;
+                }
+                saveCurrent->nextNode = numHead[i];
+                saveCurrent = numTail[i];
+            }
+            saveCurrent->nextNode = nullptr;
+
+            saveCurrent = saveNode;
+            currentNode = randHead->nextNode;
+            while (saveCurrent != nullptr)
+            {
+                if (saveCurrent->num >= 0)
+                {
+                    currentNode->num = saveCurrent->num;
+                    currentNode = currentNode->nextNode;
+                    saveCurrent = saveCurrent->nextNode;
+                }
+                else
+                {
+                    saveCurrent = saveCurrent->nextNode;
+                }
+            }
+
+            // savenode 메모리 해제
+            while (saveCurrent != nullptr)
+            {
+                node *temp = saveCurrent;
+                saveCurrent = saveCurrent->nextNode;
+                delete temp;
+            }
+
+
         }
-        cout << endl;
-
-        cursor = radixSortCompleted; // 동적 메모리 할당 해제
-        while (cursor != nullptr)
+        // 정렬 후 출력
+        cout << "정렬 후->";
+        currentNode = randHead->nextNode;
+        while (currentNode != nullptr)
         {
-            Node *temp = cursor;
-            cursor = cursor->getNext();
+            cout << currentNode->num << " ";
+            currentNode = currentNode->nextNode;
+        }
+        cout << "\n\n";
+        // 메모리 해제
+        currentNode = randHead;
+        while (currentNode != nullptr)
+        {
+            node *temp = currentNode;
+            currentNode = currentNode->nextNode;
             delete temp;
         }
-        j++;
     }
+
+    return 0;
 }
