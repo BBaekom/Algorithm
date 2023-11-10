3d8e532f85dd5a2d4d417c85cc60a8d1215d2b3d
Pig <min208406@naver.com>
2023-11-10 15:24:11 +0900

    알고과제4

--- 20201492_4.cpp
+++ 20201492_4.cpp
@@ -3,55 +3,237 @@
 #include <ctime>
 using namespace std;
 
-int select(int *, int, int, int);
-int partition(int *, int, int);
-// void linearSelet(int *, int, int, int);
-// void heapSort();
+int select(int[], int, int, int);
+int partition(int[], int, int);
+int linearSelect(int[], int, int, int);
+void heapSort(int[], int);
+void buildHeap(int[], int);
+void heapify(int[], int, int);
 
-int main(void) {
+#define NUM 100
+
+void makeRandNum(int randArr[], int num)
+{
     srand((unsigned int)time(NULL));
-    int randArr[100000];
-    int repeatCount;
-    cout << "Repeat Count : ";
-    cin >> repeatCount;
-    for (int i = 0; i < repeatCount; i++) {
-        for (int i = 0; i < 100000; i++) {
-            randArr[i] = rand();
-        }
-        int q = select(randArr, 0, 99999, 50000);
-        cout << q << " ";
+    for (int i = 0; i < num; i++)
+    {
+        randArr[i] = rand() % 100;
     }
 }
 
-int select(int * randArr, int first, int last, int i_Small) {
+int main(void)
+{
+    clock_t start, finish;
+    double duration;
+
+    // // 5-1 코드
+    // int randArr[NUM];
+    // int repeatCount;
+    // cout << "Repeat Count : ";
+    // cin >> repeatCount;
+    // makeRandNum(randArr, NUM);
+    // start = clock();
+    // for (int i = 0; i < repeatCount; i++) {
+    //     for (int k = 1; k <= (NUM / 2); k++) {
+    //         int q = select(randArr, 0, NUM - 1, k);
+    //         // cout << q << " ";
+    //     }
+    // }
+
+    // 5-2 코드
+    int randArr2[NUM];
+    int repeatCount;
+    cout << "Repeat Count : ";
+    cin >> repeatCount;
+    makeRandNum(randArr2, NUM);
+
+    cout << endl;
+    start = clock();
+    for (int i = 0; i < repeatCount; i++)
+    {
+        for (int k = 1; k <= (NUM / 2); k++) {
+            int q = linearSelect(randArr2, 0, NUM - 1, NUM / 2);
+        }
+    }
+
+    finish = clock();
+
+    duration = (double)(finish - start) / CLOCKS_PER_SEC;
+    cout << endl;
+    cout << duration << "초" << endl;
+
+    return 0;
+}
+
+// select 함수
+int select(int randArr[], int first, int last, int i_Small)
+{
     int partitionNum;
     int k;
-    if (first == last) { return randArr[first]; }
+    if (first == last)
+    {
+        return randArr[first];
+    }
     partitionNum = partition(randArr, first, last);
     k = partitionNum - first + 1;
-    if (i_Small < k){
+    if (i_Small < k)
+    {
         return select(randArr, first, partitionNum - 1, i_Small);
-    } else if (i_Small == k){
+    }
+    else if (i_Small == k)
+    {
         return randArr[partitionNum];
-    } else {
+    }
+    else
+    {
         return select(randArr, partitionNum + 1, last, i_Small - k);
     }
 }
 
-int partition(int * randArr, int first, int last) {
+// partition 함수
+int partition(int randArr[], int first, int last)
+{
     int key = randArr[last];
     int i = first - 1;
-    int tmp;
-    for (int j = first; j <= last - 1; j++) {
-        if (randArr[j] <= key) {
+    for (int j = first; j <= last - 1; j++)
+    {
+        if (randArr[j] <= key)
+        {
             i++;
-            int tmp = randArr[i];
-            randArr[i] = randArr[j];
-            randArr[j] = tmp;
+            swap(randArr[i], randArr[j]);
         }
     }
-    tmp = randArr[i + 1];
-    randArr[i + 1] = randArr[last];
-    randArr[last] = tmp;
+    swap(randArr[i + 1], randArr[last]);
     return i + 1;
+}
+
+// M을 찾았을 때 처음 partition
+int partition(int randArr[], int first, int last, int middleOfmiddle)
+{
+    int middleOfmiddleNumIndex;
+    for (int i = first; i <= last; i++)
+    {
+        if (randArr[i] == middleOfmiddle)
+        {
+            middleOfmiddleNumIndex = i;
+            break;
+        }
+    }
+    swap(randArr[middleOfmiddleNumIndex], randArr[last]);
+    int key = randArr[last];
+    int i = first - 1;
+    for (int j = first; j <= last - 1; j++)
+    {
+        if (randArr[j] <= key)
+        {
+            i++;
+            swap(randArr[i], randArr[j]);
+        }
+    }
+    swap(randArr[i + 1], randArr[last]);
+    return i + 1;
+}
+
+// buildHeap 함수
+void buildHeap(int randArr[], int elementNum)
+{
+    for (int i = elementNum / 2 - 1; i >= 0; i--)
+    {
+        heapify(randArr, i, elementNum - 1);
+    }
+}
+
+// heapify 함수 -> 책대로 하면 내림차순 돼서 부등호 방향 바꿈
+void heapify(int randArr[], int parent, int indexNum)
+{
+    int left = parent * 2 + 1;
+    int right = parent * 2 + 2;
+    int smaller;
+    if (right <= indexNum)
+    {
+        if (randArr[left] < randArr[right])
+        {
+            smaller = left;
+        }
+        else
+        {
+            smaller = right;
+        }
+    }
+    else if (left <= indexNum)
+    {
+        smaller = left;
+    }
+    else
+    {
+        return;
+    }
+
+    if (randArr[smaller] < randArr[parent])
+    {
+        swap(randArr[parent], randArr[smaller]);
+        heapify(randArr, smaller, indexNum);
+    }
+}
+
+// heapSort 함수
+void heapSort(int randArr[], int elementNum)
+{
+    buildHeap(randArr, elementNum);
+    for (int i = elementNum - 1; i >= 0; i--)
+    {
+        swap(randArr[i], randArr[0]);
+        heapify(randArr, 0, i - 1);
+    }
+}
+
+int total_num = NUM;
+// linearSelect 함수
+int linearSelect(int randArr[], int firstIndex, int lastIndex, int i_Small)
+{
+    int totalNum = lastIndex - firstIndex + 1;
+        // 1. 원소 개수가 5 이하일 때 select함수 사용
+    if (totalNum <= 5)
+    {
+        // cout << "원소가 5개 이하" << '\n';
+        return select(randArr, firstIndex, lastIndex, i_Small);
+    }
+    // 2. 그룹 수 구하기
+    int groupNum = (totalNum % 5 == 0) ? totalNum / 5 : totalNum / 5 + 1;
+
+    // 중앙값들을 모은 배열
+    int *middleNum = new int[groupNum];
+
+    // 3. 중앙값 처리 부분 - 배열에서 5개씩 힙정렬 해주고 middleNum에 정렬한 5개 중에서 중앙값 넣어줌. 마지막에 5개 이하의 원소가 남으면 그 중에서 중앙값 찾음
+    int j = 0;
+    for (int i = 0; i < totalNum; i += 5)
+    {
+        int groupLastIndex = min(i + 4, lastIndex);
+        int groupSize = groupLastIndex - i + 1;
+        heapSort(&randArr[i], groupSize);
+        middleNum[j++] = randArr[i + groupSize / 2];
+    }
+
+    // 4. 중앙값들의 중앙값 찾기
+    int middleOfmiddleNum = linearSelect(middleNum, 0, groupNum - 1, (groupNum / 2) + 1);
+    delete[] middleNum;
+
+    // 5. M을 기준삼아 전체 원소를 분할한다.
+    int partitionNum = partition(randArr, firstIndex, lastIndex, middleOfmiddleNum); // 새로 구한 M 인덱스 = partitionNum
+    int k = partitionNum - firstIndex + 1; // M은 배열에서 k번째의 수이다.
+   
+    if (i_Small == k)
+    {
+        return randArr[partitionNum];
+    } // k와 찾고자하는 i_Small 번째 수가 같으면 바로 그 수를 리턴
+    else if (i_Small < k)
+    {
+        total_num = partitionNum - firstIndex;
+        return linearSelect(randArr, firstIndex, partitionNum - 1, i_Small);
+    }
+    else
+    {
+        total_num = lastIndex - partitionNum;
+        return linearSelect(randArr, partitionNum + 1, lastIndex, i_Small - k);
+    }
 }
--- hello.cpp
+++ hello.cpp
@@ -1,161 +1,194 @@
+#include <ctime>
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
-#include <cmath>
 
 using namespace std;
-int n, m;
-
-// 연결리스트를 만들기 위한 구조체
-struct node
+int LinearEvenInWorstSelect(int A[], int p, int r, int i);
+void Heapify(int A[], int k, int n)
 {
-    int num;
-    node *nextNode;
-};
-
-// 값을 받아 새로운 노드를 생성해서 현재 노드에 연결해주는 함수
-node *makeAndLinkNode(node *currentNode, int num)
-{
-    //new 연산자 사용
-    node *newNode = new node;
-    newNode->num = num;
-    newNode->nextNode = nullptr;
-
-    if (currentNode != nullptr)
+    int left = 2 * k;
+    int right = 2 * k + 1;
+    int smaller;
+    if (right <= n)
     {
-        currentNode->nextNode = newNode;
+        if (A[left] < A[right])
+        {
+            smaller = left;
+        }
+        else
+        {
+            smaller = right;
+        }
     }
-    return newNode;
+    else if (left <= n)
+    {
+        smaller = left;
+    }
+    else
+    {
+        return;
+    }
+    if (A[smaller] < A[k])
+    {
+        swap(A[k], A[smaller]);
+        Heapify(A, smaller, n);
+    }
+}
+void BuildHeap(int A[], int n)
+{
+    for (int i = n / 2; i >= 1; i--)
+    {
+        Heapify(A, i, n);
+    }
 }
 
-int main(void)
+void HeapSort(int A[], int n)
+{
+    BuildHeap(A, n);
+
+    for (int i = n; i >= 1; i--)
+    {
+        swap(A[0], A[i]);
+        n--;
+        Heapify(A, 0, n);
+    }
+}
+
+int SortAndGetMedian(int A[], int p, int r)
+{
+    int len = r - p + 1;
+    int *tmp = new int[r - p + 1 + 1];
+    for (int i = 0; i < len; i++)
+    {
+        tmp[i + 1] = A[p + i];
+    }
+    // 알고리즘 3에서 힙정렬 알고리즘으로 정렬한 후 중앙값을 구한다.
+    // 교재의 힙정렬 알고리즘 사용
+    HeapSort(tmp, len + 1);
+    for (int i = 0; i < len; i++)
+    {
+        A[p + i] = tmp[i];
+    }
+    delete[] tmp;
+    return (p + r) / 2;
+}
+
+int Partition(int A[], int p, int r)
+{
+    int x = A[r];
+    int i = p - 1;
+    for (int j = p; j < r; j++)
+    {
+        if (A[j] <= x)
+        {
+            swap(A[++i], A[j]);
+        }
+    }
+    swap(A[i + 1], A[r]);
+    return i + 1;
+}
+
+int ChoosePivot(int A[], int p, int r)
+{
+    if (r - p < 5)
+    {
+        return SortAndGetMedian(A, p, r);
+    }
+
+    int cursor = p;
+    // 2. 전체 원소를 5개씩의 원소를 가진 n/5개의 그룹으로 나눈다.
+    // min 사용으로 5개 미만인 그룹인 경우 처리
+    for (int i = p; i <= r; i += 5)
+    {
+        // 3. 각 그룹에서 중앙값(원소가 5개이면 3번째 원소)을 찾는다.
+        int median_pos = SortAndGetMedian(A, i, min(r, i + 4));
+        swap(A[median_pos], A[cursor]);
+        cursor++;
+    }
+    // 4. m1, m2 ... 들의 중앙값 M을 재귀적으로 구한다.
+    return LinearEvenInWorstSelect(
+        A, p, cursor - 1,
+        (cursor + p - 1) / 2); // 원소의 총수가 짝수면 두 중앙값 중 임의로
+                               // 선택한다. (작은 index 선택)
+}
+int AvgLinearSelect(int A[], int p, int r, int i)
+{
+    int q = Partition(A, p, r);
+    int k = q - p + 1;
+    if (i < k)
+    {
+        return AvgLinearSelect(A, p, q - 1, i);
+    }
+    else if (i == k)
+    {
+        return q;
+    }
+    else
+    {
+        return AvgLinearSelect(A, q + 1, r, i - k);
+    }
+}
+
+int LinearEvenInWorstSelect(int A[], int p, int r, int i)
+{
+    if (p == r)
+    {
+        return p;
+    }
+    // 1. 원소의 총수가 5개 이하면 i번째 원소를 찾고 알고리즘을 끝낸다.
+    if (r - p + 1 <= 5)
+    {
+        cout << "TT" << p << "TT" << r << "TT" << A[p] << "TT" << A[r] << "TT"
+             << endl;
+        return AvgLinearSelect(A, p, r, i); // 5-1 알고리즘 사용
+    }
+
+    int M_pos = ChoosePivot(A, p, r);
+    // 5. M을 기준원소로 삼아 전체원소를 분할한다. (교재의 partition 함수 사용)
+    swap(A[M_pos], A[r]);
+    int partition_result = Partition(A, p, r);
+
+    // 6. 분할된 두 그룹 중 적합한 쪽을 선택해 재귀적으로 반복
+    if (partition_result == i)
+    {
+        return partition_result;
+    }
+    else if (partition_result > i)
+    {
+        return LinearEvenInWorstSelect(A, p, partition_result - 1, i);
+    }
+    else if (partition_result < i)
+    {
+        return LinearEvenInWorstSelect(A, partition_result + 1, r, i);
+    }
+    return -1;
+}
+
+int main()
 {
     srand(time(NULL));
 
-    cout << "n:";
-    cin >> n;
-    cout << "m:";
-    cin >> m;
+    clock_t start, finish;
+    double duration;
 
-    for (int i = 0; i < m; i++)
+    int ARR_LEN = 100000;
+    int ITER_COUNT = 1000;
+
+    int A[100000];
+
+    start = clock();
+    for (; ITER_COUNT > 0; ITER_COUNT--)
     {
-        node *randHead = makeAndLinkNode(nullptr, -1);
-        node *currentNode = randHead;
-
-        // 각 연결리스트의 헤드를 저장하는 크기가 10인 포인터 배열을 생성
-        node *numHead[10] = {nullptr};
-
-        // 각 연결리스트의 테일을 저장하는 크기가 10인 포인터 배열을 생성
-        node *numTail[10] = {nullptr};
-
-        // 랜덤 숫자 생성, 동적 메모리 할당을 통한 연결리스트 생성
-        for (int j = 0; j < n; j++)
+        for (int i = 0; i < ARR_LEN; i++)
         {
-            int a = rand() % 10000;
-            currentNode = makeAndLinkNode(currentNode, a);
+            A[i] = rand() % ARR_LEN;
         }
-        cout<<"정렬 전->";
-        currentNode = randHead->nextNode;
-        while (currentNode != nullptr)
-        {
-            cout << currentNode->num << " ";
-            currentNode = currentNode->nextNode;
-        }
-        cout << "\n";
 
-        // 기수정렬 시행
-
-        for (int j = 1; j < 5; j++)
-        {
-
-            // 각 자릿수에 해당하는 10개의 연결리스트를 동적 메모리 할당으로 생성
-            for (int j = 0; j < 10; j++)
-            {
-                numHead[j] = new node;
-                numHead[j]->num = -1;
-                numHead[j]->nextNode = nullptr;
-                numTail[j] = numHead[j];
-            }
-
-            //몇번째 링크드리스트에 들어갈건지 구하기
-            currentNode = randHead->nextNode;
-            int nanoogi = pow(10, j);
-            int makeNanoogi = pow(10, j - 1);
-            while (currentNode != nullptr)
-            {
-                if (currentNode->num == -1)
-                {
-                    currentNode = currentNode->nextNode;
-                    continue;
-                }
-                int nodeWhere = (currentNode->num) % nanoogi / makeNanoogi;
-                makeAndLinkNode(numTail[nodeWhere], currentNode->num);
-                numTail[nodeWhere] = numTail[nodeWhere]->nextNode;
-                numTail[nodeWhere]->nextNode = nullptr;
-                currentNode = currentNode->nextNode;
-            }
-
-
-
-            // tail과 head 연결
-            node *saveNode = makeAndLinkNode(nullptr, -1);
-            node *saveCurrent = saveNode;
-
-            for (int i = 0; i < 10; i++)
-            {
-                if (numTail[i]->num == -1)
-                {
-                    continue;
-                }
-                saveCurrent->nextNode = numHead[i];
-                saveCurrent = numTail[i];
-            }
-            saveCurrent->nextNode = nullptr;
-
-            saveCurrent = saveNode;
-            currentNode = randHead->nextNode;
-            while (saveCurrent != nullptr)
-            {
-                if (saveCurrent->num >= 0)
-                {
-                    currentNode->num = saveCurrent->num;
-                    currentNode = currentNode->nextNode;
-                    saveCurrent = saveCurrent->nextNode;
-                }
-                else
-                {
-                    saveCurrent = saveCurrent->nextNode;
-                }
-            }
-
-            // savenode 메모리 해제
-            while (saveCurrent != nullptr)
-            {
-                node *temp = saveCurrent;
-                saveCurrent = saveCurrent->nextNode;
-                delete temp;
-            }
-
-
-        }
-        // 정렬 후 출력
-        cout << "정렬 후->";
-        currentNode = randHead->nextNode;
-        while (currentNode != nullptr)
-        {
-            cout << currentNode->num << " ";
-            currentNode = currentNode->nextNode;
-        }
-        cout << "\n\n";
-        // 메모리 해제
-        currentNode = randHead;
-        while (currentNode != nullptr)
-        {
-            node *temp = currentNode;
-            currentNode = currentNode->nextNode;
-            delete temp;
-        }
+        cout << A[LinearEvenInWorstSelect(A, 0, ARR_LEN, 49999)] << ", ";
     }
+
+    finish = clock();
+    duration = (double)(finish - start) / CLOCKS_PER_SEC;
+    cout << duration << "초" << endl;
 
     return 0;
 }
